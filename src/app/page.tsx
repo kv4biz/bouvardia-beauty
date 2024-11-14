d7ad41ff80625befbbf3a158652a973406dbe0b9
kv4biz <kv4biz@gmail.com>
2024-11-14 07:07:23 +0100

    code fix for the double scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full overflow-x-hidden flex flex-col justify-center ">
+    <div className="min-h-screen w-full flex flex-col justify-center ">
       <Navbar />
-      <HomeBanner />
-      <HomeBanner />
       <HomeBanner />
     </div>
   );
