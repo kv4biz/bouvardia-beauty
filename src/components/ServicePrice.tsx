be49bd0ebf3c9f0a01271e6cabfb2a46bf24c671
kv4biz <kv4biz@gmail.com>
2025-03-07 19:03:46 +0100

    Additon of service, about and portfolio pages3

--- src/components/ServicePrice.tsx
+++ src/components/ServicePrice.tsx
@@ -7,7 +7,7 @@ import { Button } from "./ui/button";
 const ServicePrice = () => {
   return (
     <div className="w-full py-10 md:py-16">
-      <div className="container mx-auto flex flex-col gap-2 md:gap-4 px-4 md:px-10 overflow-hidden">
+      <div className="container mx-auto flex flex-col gap-2 md:gap-4 px-4 overflow-hidden">
         <div className="flex flex-col gap-2 items-center justify-center text-center">
           <h1 className="IBMPlexMono font-medium text-3xl">
             Our Services & Pricing
@@ -17,13 +17,13 @@ const ServicePrice = () => {
             eiusmodte porincididunt ut laboreua.
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           {Services.map((service) => (
             <div
               key={service.id}
-              className="flex items-center justify-between bg-[#D0A88D]/20 rounded-sm w-full p-4"
+              className="flex items-center justify-between bg-[#D0A88D]/20 rounded-sm w-full p-4 md:p-6 md:px-10"
             >
-              <div className="flex items-center gap-2">
+              <div className="flex flex-col md:flex-row items-start md:items-center gap-4">
                 <div>
                   <Image
                     src={service.img}
@@ -37,7 +37,7 @@ const ServicePrice = () => {
                   <p className="pathwayGothicOne text-[20px]">
                     {service.title}
                   </p>
-                  <p className="text-sm inter text-[#676565] max-w-xs">
+                  <p className="text-xs  inter text-[#676565] max-w-[240px]">
                     {service.desc.split(" ").length > 16
                       ? service.desc.split(" ").slice(0, 16).join(" ") + "..."
                       : service.desc}
@@ -50,10 +50,14 @@ const ServicePrice = () => {
                   </Link>
                 </div>
               </div>
-              <div className="border-l-2 border-black/20 p-2 flex flex-col gap-2">
-                <p>{service.duration}</p>
-                <p>{service.price}</p>
-                <Button size={"sm"} variant={"darkghost"}>
+              <div className="border-l-2 border-black/20 p-2 py-10 flex flex-col gap-2">
+                <p className="text-xs inter font-light">{service.duration}</p>
+                <p className="text-sm inter font-medium">{service.price}</p>
+                <Button
+                  size={"sm"}
+                  variant={"darkghost"}
+                  className="mt-3 text-xs inter"
+                >
                   <Link href={service.link}>Book Now</Link>
                 </Button>
               </div>
